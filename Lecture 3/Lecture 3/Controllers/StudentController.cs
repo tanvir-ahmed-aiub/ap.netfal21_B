f4af47dfb3580963340452d45d6c5788b735f15a
Tanvir <tanvir.ahmeed.aiub@gmail.com>
2021-09-21 15:51:17 +0600

    Lecture 3

---
+++ Lecture 3/Lecture 3/Controllers/StudentController.cs
@@ -0,0 +1,61 @@
+﻿using Lecture_3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+namespace Lecture_3.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            string connString = @"Server=DESKTOP-NM0ID5F\SQLEXPRESS; Database=UMS; Integrated Security=true";
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+            string query = "select * from Students";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Student> students = new List<Student>();
+            while (reader.Read()) {
+                Student s = new Student() { 
+
+                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                    Name = reader.GetString(reader.GetOrdinal("Name")),
+                    Dob = reader.GetString(reader.GetOrdinal("Dob")),
+                    Cgpa = (float)reader.GetDouble(reader.GetOrdinal("Cgpa"))
+
+                };
+                students.Add(s);
+            }
+
+            conn.Close();
+            return View(students);
+        }
+        [HttpGet]
+        public ActionResult Create() 
+        {
+            Student s = new Student();
+            return View(s);
+        }
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            if (ModelState.IsValid) {
+                string connString = @"Server=DESKTOP-NM0ID5F\SQLEXPRESS; Database=UMS; Integrated Security=true";
+                SqlConnection conn = new SqlConnection(connString);
+                conn.Open();
+                string query = String.Format("insert into Students values ('{0}','{1}','{2}',0.0)", s.Name, s.Dob, s.Gender);
+                SqlCommand cmd = new SqlCommand(query, conn);
+                int r = cmd.ExecuteNonQuery();
+                conn.Close();
+                return RedirectToAction("Index");
+            }
+            return View(s);
+        }
+
+    }
+
+}
---
+++ Lecture 3/Lecture 3/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lecture_3.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Please put your name")]
+        [StringLength(10,ErrorMessage ="Name should not exceed 10 charcter")]
+        [MinLength(5)]
+        public string Name { get; set; }
+        [Required]
+        public string Dob { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        public Double Cgpa { get; set; }
+    }
+}
